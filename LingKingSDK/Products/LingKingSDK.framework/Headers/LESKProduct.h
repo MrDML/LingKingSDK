b353af0fd6cd01ab4fe9a3ca944bfd4a77f88125
MrDML <dml1630@163.com>
2020-07-08 11:33:53 +0800

    update 0.1.42

--- LingKingSDK/Products/LingKingSDK.framework/Headers/LESKProduct.h
+++ LingKingSDK/Products/LingKingSDK.framework/Headers/LESKProduct.h
@@ -12,10 +12,15 @@
 
 @interface LESKProduct : NSObject
 - (instancetype)initWithArray:(SKProduct *)product;
+/// 商品ID
 @property (nonatomic,copy) NSString *productId;
+/// 商品描述
 @property (nonatomic,copy) NSString *desc;
+/// 商品标题
 @property (nonatomic,copy) NSString *localizedTitle;
+/// 本地描述
 @property (nonatomic,copy) NSString *localizedDescription;
+/// 价格
 @property (nonatomic,strong) NSDecimalNumber *price;
 @end
 
